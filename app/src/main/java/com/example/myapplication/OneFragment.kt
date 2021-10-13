ffcff8a3c1a199177be5a24f4146a373ba9589d5
nzechu <arinzendubuishi@gmail.com>
2021-10-13 19:14:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/OneFragment.kt
@@ -0,0 +1,58 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import com.example.myapplication.databinding.FragmentFourBinding
+import com.example.myapplication.databinding.FragmentOneBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [OneFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class OneFragment : Fragment() {
+    lateinit var binding: FragmentOneBinding
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController: NavController = Navigation.findNavController(view)
+        val button : Button = view.findViewById(R.id.startedButton)
+        button.setOnClickListener{
+            navController.navigate(R.id.action_oneFragment_to_twoFragment)
+        }
+    }
+    }
+
+
---
+++ app/src/main/java/com/example/myapplication/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.myapplication.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent (this, MainActivity::class.java)
+            finish()
+            startActivity(intent)}, 3000)
+
+    }
+
+
+}
